a572c327fc8596c788ef9b0e8246493bcecbbac5
David Jandron <jandron1211@gmail.com>
2023-01-09 14:44:12 -0800

    add addition, subtraction, multiplacation, division functionality in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
 // business logic
 function add(number1, number2) {
-  return number1 + number2;
+  return "The sum is " + parseInt(number1 + number2) + ".";
 }
+
+function subtract(number1, number2) {
+  return "The difference is " + parseInt(number1 - number2) + ".";
+}
+
+function multiply(number1, number2) {
+  return "The product is " + parseInt(number1 * number2) + ".";
+}
+
+function divide(number1, number2) {
+  return "The quotient is " + parseInt(number1 / number2) + "";
+}
+
+
+
 
 //user interface logic
 const number1 = parseInt(prompt("enter a number:"));
 const number2 = parseInt(prompt("enter another number:"));
 window.alert(add(number1, number2));
+
+window.alert(subtract(number1, number2));
+
+window.alert(multiply(number1, number2));
+
+window.alert(divide(number1, number2));
